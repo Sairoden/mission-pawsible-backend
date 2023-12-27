6007a959648928c46b73335eb8e2a5c52efb1c0e
Sairoden <odingandarosa@gmail.com>
2023-12-27 18:03:28 +0800

    Change NewsAPI and fetch request

--- controllers/resourceController.js
+++ controllers/resourceController.js
@@ -2,18 +2,16 @@ require("dotenv").config({ path: "./.env" });
 
 const getAllResources = async (req, res) => {
   try {
-    let { page } = req.query;
-
-    let apiUrl = `https://newsdata.io/api/1/news?apikey=${process.env.NEWS_API}&qInTitle=pets&language=en,pi&category=education,entertainment,food,health`;
-    if (page) {
-      apiUrl += `&page=${page}`;
-    }
+    const apiUrl = `https://newsapi.org/v2/everything?qInTitle="pets"&sortBy=popularity&language=en&apiKey=${process.env.ORIGINAL_NEWS_API}`;
 
     const response = await fetch(apiUrl);
 
     const data = await response.json();
 
-    return res.status(200).send(data);
+    if (data.status !== "ok")
+      throw new Error("Could not load resources. Please try again later.");
+
+    return res.status(200).send({ resources: data?.articles });
   } catch (err) {
     return res.status(400).send(err.message);
   }
